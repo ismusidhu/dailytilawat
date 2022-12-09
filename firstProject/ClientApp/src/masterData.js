00ed0b0d6998f6f47de003b178a8983707890158
Ismail S <ismail@infiswift.tech>
2022-12-09 17:48:14 +0530

    daily tilawat code added

---
+++ firstProject/ClientApp/src/masterData.js
@@ -0,0 +1,31 @@
+﻿export const qasaaid = [{
+    name: 'Ya Sayyeda',
+    baitCount: 52,
+    hasFehwa: true
+}, {
+    name: 'Fulkul Husain',
+    baitCount: 52,
+    hasFehwa: true
+}, {
+    name: 'Zikrul Husain',
+    baitCount: 33,
+    hasFehwa: true
+}, {
+    name: 'Munajaat 1443',
+    baitCount: 52,
+    hasFehwa: true
+}];
+
+export const userConfig = {
+    dailyTilaawat: [{
+        name: 'Ya Sayyeda',
+        baitCount: 1,
+        showFehwa: true,
+        currentBait: 4
+    }, {
+        name: 'Fulkul Husain',
+        baitCount: 1,
+        showFehwa: true,
+        currentBait: 3
+    }]
+}
